45ddce6bbcbe611f5a5639268bb34cd66ea54e9f
Danny Parker <danny.parker@myunidays.com>
2019-07-03 13:17:08 +0100

    Add alternative method and encapsulate

---
+++ AthenaClient/Method2.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.Athena;
+using Amazon.Athena.Model;
+
+namespace AthenaClient
+{
+    public class Method2
+    {
+        private readonly AmazonAthenaClient _amazonAthenaClient;
+
+        public Method2(AmazonAthenaClient amazonAthenaClient) =>
+            _amazonAthenaClient = amazonAthenaClient;
+
+        public async Task Execute()
+        {
+            using (var client = _amazonAthenaClient)
+            {
+                var qContext = new QueryExecutionContext { Database = "dannys_cool_database" };
+                var resConf = new ResultConfiguration { OutputLocation = "dannys_cool_bucket" };
+
+                Console.WriteLine("Created Athena Client");
+                await Run(client, qContext, resConf);
+            }
+        }
+
+        private static async Task Run(IAmazonAthena client, QueryExecutionContext qContext, ResultConfiguration resConf)
+        {
+            /* Execute a simple query on a table */
+            var qReq = new StartQueryExecutionRequest
+            {
+                QueryString = "SELECT * FROM cloudtrail_logs limit 10;",
+                QueryExecutionContext = qContext,
+                ResultConfiguration = resConf
+            };
+
+            try
+            {
+                /* Executes the query in an async manner */
+                var qRes = await client.StartQueryExecutionAsync(qReq);
+                /* Call internal method to parse the results and return a list of key/value dictionaries */
+                var items = await GetQueryExecution(client, qRes.QueryExecutionId);
+                foreach (var item in items)
+                {
+                    foreach (var (key, value) in item)
+                    {
+                        Console.WriteLine("Col: {0}", key);
+                        Console.WriteLine("Val: {0}", value);
+                    }
+                }
+            }
+            catch (InvalidRequestException e)
+            {
+                Console.WriteLine("Run Error: {0}", e.Message);
+            }
+        }
+
+        private static async Task<List<Dictionary<string, string>>> GetQueryExecution(IAmazonAthena client, string id)
+        {
+            var items = new List<Dictionary<string, string>>();
+            QueryExecution q = null;
+            /* Declare query execution request object */
+            var qReq = new GetQueryExecutionRequest { QueryExecutionId = id };
+            /* Poll API to determine when the query completed */
+            do
+            {
+                try
+                {
+                    var results = await client.GetQueryExecutionAsync(qReq);
+                    q = results.QueryExecution;
+                    Console.WriteLine("Status: {0}... {1}", q.Status.State, q.Status.StateChangeReason);
+
+                    await Task.Delay(5000); //Wait for 5sec before polling again
+                }
+                catch (InvalidRequestException e)
+                {
+                    Console.WriteLine("GetQueryExec Error: {0}", e.Message);
+                }
+            } while (q != null && (q.Status.State == "RUNNING" || q.Status.State == "QUEUED"));
+
+            if (q != null)
+                Console.WriteLine("Data Scanned for {0}: {1} Bytes", id, q.Statistics.DataScannedInBytes);
+
+            /* Declare query results request object */
+            var resReq = new GetQueryResultsRequest()
+            {
+                QueryExecutionId = id,
+                MaxResults = 10
+            };
+
+            GetQueryResultsResponse resResp;
+            /* Page through results and request additional pages if available */
+            do
+            {
+                resResp = await client.GetQueryResultsAsync(resReq);
+                /* Loop over result set and create a dictionary with column name for key and data for value */
+                foreach (var row in resResp.ResultSet.Rows)
+                {
+                    var dict = new Dictionary<string, string>();
+                    for (var i = 0; i < resResp.ResultSet.ResultSetMetadata.ColumnInfo.Count; i++)
+                    {
+                        dict.Add(resResp.ResultSet.ResultSetMetadata.ColumnInfo[i].Name, row.Data[i].VarCharValue);
+                    }
+                    items.Add(dict);
+                }
+
+                if (resResp.NextToken != null)
+                    resReq.NextToken = resResp.NextToken;
+
+            } while (resResp.NextToken != null);
+
+            /* Return List of dictionary per row containing column name and value */
+            return items;
+        }
+    }
+}
--- AthenaClient/Program.cs
+++ AthenaClient/Program.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Amazon;
 using Amazon.Athena;
-using Amazon.Athena.Model;
 
 namespace AthenaClient
 {
@@ -14,123 +10,11 @@ namespace AthenaClient
         {
             var athenaClient = new AmazonAthenaClient(new AmazonAthenaConfig { RegionEndpoint = RegionEndpoint.EUWest1 });
 
-            var queryExecutionId = await SubmitAthenaQuery(athenaClient);
+            var method1 = new Method1(athenaClient);
+            await method1.Execute();
 
-            await WaitForQueryToComplete(athenaClient, queryExecutionId);
-
-            await ProcessResultRows(athenaClient, queryExecutionId);
-        }
-
-        private static async Task<string> SubmitAthenaQuery(AmazonAthenaClient athenaClient)
-        {
-            // Set the DB here
-            var queryExecutionContext = new QueryExecutionContext { Database = "dannys_cool_database" };
-
-            // Specify where the result should go in S3
-            var resultConfiguration = new ResultConfiguration { OutputLocation = "dannys_cool_bucket" };
-
-            // Send Execution Request to Athena to start query
-            var startQueryExecutionRequest = new StartQueryExecutionRequest()
-            {
-                QueryString = "select count(*) as Count from dannys_cool_table where danny == lad",
-                QueryExecutionContext = queryExecutionContext,
-                ResultConfiguration = resultConfiguration
-            };
-
-            var startQueryExecutionResponse = await athenaClient.StartQueryExecutionAsync(startQueryExecutionRequest);
-            return startQueryExecutionResponse.QueryExecutionId;
-        }
-
-        private static async Task WaitForQueryToComplete(IAmazonAthena athenaClient, string queryExecutionId)
-        {
-            var getQueryExecutionRequest = new GetQueryExecutionRequest { QueryExecutionId = queryExecutionId };
-
-            var queryStillRunning = true;
-
-            // Poll Athena for completion state of query.
-            while (queryStillRunning)
-            {
-                var getQueryExecutionResponse = await athenaClient.GetQueryExecutionAsync(getQueryExecutionRequest);
-                var queryState = getQueryExecutionResponse.QueryExecution.Status.State;
-
-                if (queryState == QueryExecutionState.FAILED)
-                    throw new Exception("Query Failed to run with Error Message: " + getQueryExecutionResponse.QueryExecution.Status.StateChangeReason);
-                if (queryState == QueryExecutionState.CANCELLED)
-                    throw new Exception("Query was cancelled.");
-                if (queryState == QueryExecutionState.SUCCEEDED)
-                    queryStillRunning = false;
-                else
-                {
-                    // Sleep an amount of time before retrying again.
-                    Thread.Sleep(TimeSpan.FromMilliseconds(1000));
-                }
-
-                Console.WriteLine("Current Status is: " + queryState);
-            }
-        }
-
-        private static async Task ProcessResultRows(AmazonAthenaClient athenaClient, string queryExecutionId)
-        {
-            // Max Results can be set but if its not set, it will choose the maximum page size
-            var getQueryResultsRequest = new GetQueryResultsRequest { QueryExecutionId = queryExecutionId };
-
-            var getQueryResultsResponse = await athenaClient.GetQueryResultsAsync(getQueryResultsRequest);
-
-            while (true)
-            {
-                var results = getQueryResultsResponse.ResultSet.Rows;
-                foreach (var row in results)
-                {
-                    // Process the row. The first row of the first page holds the column names.
-                    ProcessRow(row, getQueryResultsResponse.ResultSet.ResultSetMetadata.ColumnInfo);
-                }
-
-                // If nextToken is null, there are no more pages to read. Break out of the loop.
-                if (string.IsNullOrEmpty(getQueryResultsResponse.NextToken))
-                    break;
-
-                getQueryResultsRequest.NextToken = getQueryResultsResponse.NextToken;
-                getQueryResultsResponse = await athenaClient.GetQueryResultsAsync(getQueryResultsRequest);
-            }
-        }
-
-        private static void ProcessRow(Row row, IEnumerable<ColumnInfo> columnInfoList)
-        {
-            foreach (var column in columnInfoList)
-            {
-                switch (column.Type)
-                {
-                    case "varchar":
-                        // Convert and Process as String
-                        break;
-                    case "tinyint":
-                        // Convert and Process as tinyint
-                        break;
-                    case "smallint":
-                        // Convert and Process as smallint
-                        break;
-                    case "integer":
-                        // Convert and Process as integer
-                        break;
-                    case "bigint":
-                        // Convert and Process as bigint
-                        break;
-                    case "double":
-                        // Convert and Process as double
-                        break;
-                    case "boolean":
-                        // Convert and Process as boolean
-                        break;
-                    case "date":
-                        // Convert and Process as date
-                        break;
-                    case "timestamp":
-                        // Convert and Process as timestamp
-                        break;
-                    default:
-                        throw new Exception($"Unexpected Type: {column.Type}");
-                }
-            }
+            var method2 = new Method2(athenaClient);
+            await method2.Execute();
         }
     }
 }
